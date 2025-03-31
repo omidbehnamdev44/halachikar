eaabffb47e6e8e2c674a0ffa74fb02ea94298234
omid <xdvdxzv@gmail.com>
2025-03-31 07:48:41 -0800

    add modules - #2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,10 @@ dependencyResolutionManagement {
 
 rootProject.name = "Halachikar"
 include(":app")
+include(":feature:home")
+include(":feature:addtask")
+include(":data:local")
+include(":data:repository")
+include(":domain")
+include(":core:utils")
+include(":core:resource")
